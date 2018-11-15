abbad386d527a967e1a32725378d328ed77576f7
William J. Peria <wperia@fredhutch.org>
2018-11-15 07:06:37 -0800

    Added a public method to get_new_data. Open app with plots already plotted. Trigger a plot_update in view when a different feature map is selected. Almost added a New Data button, then panicked and commented it out oops. Alm

--- model_inspect.py
+++ model_inspect.py
@@ -18,7 +18,7 @@ from matplotlib.figure import Figure
 import dldb
 from dldb import dlTile
 
-FEATURE_MAPS = 'wtf?!'
+FEATURE_MAPS = 'nothing to see here yet'  # a global to hold maps obtained via hook functions. 
 
 class Model:
     def __init__(self):
@@ -53,6 +53,11 @@ class Model:
 
 #        self.input, self.masks = self.grab_new_batch(N=[318, 376, 448, 819, 979],\
 #                                         maskfile='test3_cancer.tif')
+
+        self.get_new_data()
+
+        
+    def get_new_data(self):
         Nlist = list(np.random.randint(0,high=1260,size=self.batch_size))
         self.input, self.masks = self.grab_new_batch(N=Nlist,\
                                          maskfile='test3_cancer.tif')
@@ -90,7 +95,7 @@ class Model:
             fm = self.feature_display[self.icurrent,:,:]
             return fm
         else:
-            return np.zeros((2,2))
+            return np.asarray([[1,0],[0,1]])
     
     def get_model(self):
         return self.model
@@ -134,8 +139,11 @@ class Model:
                
         assert(nx==ny)
         sqsize = nx
-        brdr = 1
-        disp = 1 + np.zeros((nexamp,nrows*(sqsize+brdr),ncols*(sqsize+brdr)))
+        brdr = nx // 8
+        if brdr == 0:
+            brdr = 1
+            
+        disp = np.max(feat) + np.zeros((nexamp,nrows*(sqsize+brdr),ncols*(sqsize+brdr)))
         
         for examp in range(nexamp):
             for i in range(ndepth):
@@ -275,11 +283,13 @@ class View:
         self.model = model
         self.net = model.get_model()
         
-        self.fig = Figure(figsize=(7.5, 4), dpi=80)
+#        self.fig = Figure(figsize=(7.5, 4), dpi=80) # original
+        self.fig = Figure(figsize=(16, 8), dpi=80)
         self.ax0 = self.fig.subplots(2,2)
 #        self.ax0 = self.fig.add_axes((0.05, .05, .90, .90), \
 #                                     facecolor=(.75, .75, .75), frameon=False)
         self.frame.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
+
  #------------------------       
         self.frame2 = Tk.Frame(root)
         self.frame2.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
@@ -313,16 +323,31 @@ class View:
                      if len(module._modules) == 0]
         self.v = Tk.StringVar(master=self.frame2,name="module")
         self.v.set(optionList[0])
+        #
+#        def callback2(*_, var=self.v):
+#            model.display_feature(var)
+#            crap = lambda *_ : self.plot()
+#            crap(*_)
+#            print('I can do two things..')
+#            
+#        def callback3(self, n,m,x, var=self.v):
+#            model.display_feature(var)
+#            self.update_plots()
+#            print('I could do three things..')
+#
         # In the next line, "callback" is the name of the function that is 
         #   called when v is changed, i.e. when the user picks a new option from 
         #   paramMenu. It has to be a lambda (an anonymous function) to accomodate
         #   the additionsl arguments that I want to pass to my display_feature code. 
         # I think that *_ represents the set of arguments that trace_add passes in 
         # the background...I don't get to control that. 
-        #
-        callback = lambda *_, var=self.v  : model.display_feature(var)
+            
+#        callback = lambda *_, var=self.v  : model.display_feature(var)
+        def callback(*_, var=self.v):
+            model.display_feature(var)
+            self.update_plots()
         self.v.trace_add("write", callback)
-        
+
         self.paramMenu = Tk.OptionMenu(self.frame2, self.v, *optionList)
         self.paramMenu.pack(side="top",fill=Tk.BOTH)
                 
@@ -333,6 +358,10 @@ class View:
         self.prevButton.pack(side="top", fill=Tk.BOTH)
         self.prevButton.bind("<Button>", self.prev)
 
+#        self.grabButton = Tk.Button(self.frame2, text="New Data")
+#        self.grabButton.pack(side="top", fill=Tk.BOTH)
+#        self.grabButton.bind("<Button>", self.grab)
+
         self.quitButton = Tk.Button(self.frame2, text="Quit")
         self.quitButton.pack(side="top", fill=Tk.BOTH)
         self.quitButton.bind("<Button>", self.quitit)
@@ -340,6 +369,8 @@ class View:
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.frame)
         self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
         self.canvas.draw()
+        
+        self.update_plots()
 
     def next(self, event):
         self.model.next_example()
@@ -348,12 +379,18 @@ class View:
     def prev(self, event):
         self.model.prev_example()
         self.plot(event)
-        
+    
+#    def grab(self, event):
+#        self.model.get_new_data()
+    
     def clear(self, event):
         self.ax0.clear()
         self.fig.canvas.draw()
 
     def plot(self, event):
+        self.update_plots()
+        
+    def update_plots(self):
 #        tile = self.model.db.get_random_tile()
         self.ax0[0,0].clear() # inexplicably began causing trouble....Oh! matplotlib was only ever imported in my hook, which is global 
 #        tile.show(self.ax0)
