7cf7dd670114ead96fbb5348e20655734997531d
William J. Peria <wperia@fredhutch.org>
2018-12-15 16:45:54 -0800

    added measure of saturation

--- tissue_percentage.py
+++ tissue_percentage.py
@@ -24,10 +24,16 @@ import pickle
 def get_tissue_percentage(H_and_E):
     nx, ny, nz = H_and_E.shape
     
-    white = np.min(H_and_E[:,:,0:3],axis=2) > (.92*255)
+    minc = np.min(H_and_E[:,:,0:3],axis=2)
+    maxc = np.max(H_and_E[:,:,0:3],axis=2)
+    
+    white = minc > (.92*255)
     white_frac = np.sum(white)/np.prod(H_and_E.shape[0:2])
     
-    return int(100-np.round(white_frac*100))
+    sat_img = (maxc - minc)/maxc
+    sat = np.mean(sat_img[white == 0])
+    
+    return int(100-np.round(white_frac*100)), int(np.round(sat*100))
         
 
 if __name__ == "__main__":
@@ -35,20 +41,20 @@ if __name__ == "__main__":
     openslide.Image.MAX_IMAGE_PIXELS = None # prevents DecompressionBomb Error
 
     dirs_to_process = bu.uichoosedirs()
-    
+    print(dirs_to_process)
     file_dict = {}
     for dir_to_process in dirs_to_process:
         files_to_process = sorted(glob.glob(dir_to_process+'/*.tif'))
-        files_to_process = [f for f in files_to_process if 'cpd' not in f]
-        print('files: ',files_to_process)
+        files_to_process = [f for f in files_to_process if '_cpd' not in f]
+        print(files_to_process)
         for file_to_process in files_to_process:
             Tissue = openslide.open_slide(file_to_process)
             allT = np.asarray(Tissue.read_region((0,0),0,Tissue.dimensions))[:,:,0:3]    
-            pct = get_tissue_percentage(allT)
+            pct, sat = get_tissue_percentage(allT)
             
             this_file = bu.just_filename(bu, file_to_process)
-            print('{:s}: {:3d}'.format(this_file, pct))
-            file_dict.update({this_file : pct})
+            print('{:s}: {:3d} tissue , {:3d} saturated'.format(this_file, pct, sat))
+            file_dict.update({this_file : (pct , sat)})
     
     output_name = 'tissue_pct_' + bu.date_for_filename() + '.dat'
     with open(output_name,'wb') as f:
