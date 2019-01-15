Added option to do 3-color lumped normalization which, while incorrect, yielded a cancer correlated feature map in the test/train data. Current just hard-coding a Model member: self.normalize_wrong.

dldb.py
@@ -425,7 +425,8 @@ class DLDB():
             return aminv
 
 
-    def feed_pytorch(self,N=None,maskfile=None,augment=False):
+    def feed_pytorch(self,N=None,maskfile=None,augment=False, \
+                     normalize_wrong=False):
         if type(N) is list:
             tiles = self.get_tile_by_number(N)
             (nx,ny,nz) = tiles[0].data.shape
@@ -444,10 +445,14 @@ class DLDB():
 #            inbatch[:,:,:,i] = (inbatch[:,:,:,i] - np.mean(inbatch[:,:,:,i]))/ \
 #            np.std(inbatch[:,:,:,i])
         
-        IB0 = np.mean(inbatch,axis=(0,1,2),dtype=np.float32)
-        dIB = np.std( inbatch,axis=(0,1,2),dtype=np.float32)
-        inbatch = (inbatch - IB0) / dIB  # per color norm, via broadcasting
-        
+        if normalize_wrong:
+            print('Wrong normalization!')
+            inbatch = (inbatch - np.mean(inbatch))/np.std(inbatch)
+        else:
+            IB0 = np.mean(inbatch,axis=(0,1,2),dtype=np.float32)
+            dIB = np.std( inbatch,axis=(0,1,2),dtype=np.float32)
+            inbatch = (inbatch - IB0) / dIB  # per color norm, via broadcasting
+            
         if augment:
             augmatinv = self.get_aug_trans(ntiles,(nx,ny))
             for i in range(ntiles):

model_inspect.py
@@ -48,6 +48,10 @@ class Model():
         #
         import fcn
         
+        self.normalize_wrong = True
+        if self.normalize_wrong:
+            print('Using incorrect 3-color lumped normalization...')
+        
         self.batch_size = batch_size
         self.GPU = GPU
         
@@ -105,6 +109,7 @@ class Model():
         self.viewers.append(v)
         
     def get_new_data(self):
+        
         if not self.maskfile:
             maskfile = 'test3_cancer.tif'
         Nlist = list(np.random.randint(0,high=1260,size=self.batch_size))
@@ -236,12 +241,14 @@ class Model():
     def get_net(self):
         return self.net
     
-    def grab_new_batch(self,N=None, maskfile = None, augment = False, boundary_kernel=None):
+    def grab_new_batch(self,N=None, maskfile = None, augment = False, \
+                       boundary_kernel=None):
 
         if N == None:
             N=list(np.random.randint(0,size=self.batch_size,high=1260))    
 
-        indata, y = self.db.feed_pytorch(N=N, maskfile=maskfile)
+        indata, y = self.db.feed_pytorch(N=N, maskfile=maskfile,\
+                                        normalize_wrong=self.normalize_wrong)
         
         if self.GPU:
             indata = indata.cuda()